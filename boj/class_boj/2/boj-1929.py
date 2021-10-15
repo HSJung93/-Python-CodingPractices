07d8be403a5d6126ae40ffb0d316318062b4653f
Hoesung Jung <jrps2212@gmail.com>
2021-10-15 10:10:29 +0900

    1 경우의 수 더함

--- boj/class_boj/2/boj-1929.py
+++ boj/class_boj/2/boj-1929.py
@@ -1,15 +1,14 @@
-import math
+def isPrime(num):
+    if num==1:
+        return False
+    else:
+        for i in range(2, int(num**0.5)+1):
+            if num%i == 0:
+                return False
+        return True
+
 M, N = map(int, input().split())
 
-def printDecimal(i):
-    notDecimal = False
-    for j in range(2, math.ceil(math.sqrt(i) + 1)):
-        if i%j == 0:
-            notDecimal = True
-            break 
-            
-    if notDecimal == False:
-        print(i)
-
 for i in range(M, N+1):
-    printDecimal(i)
+    if isPrime(i):
+        print(i)
